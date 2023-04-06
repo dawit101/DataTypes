46a6ce4e8aa6ea17183c32aaa9442822e1eca8a9
dawit101 <130009377+dawit101@users.noreply.github.com>
2023-04-06 05:31:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# <- the sign means to make a 'comment'
+
+#Fundamental Data Types
+
+int #integer means numbers
+float #Convert a string or number to a floating point number, if possible.
+bool #Returns True when the argument x is true, False otherwise
+str #Create a new string object from the given object
+list #If no argument is given, the constructor creates a new empty list. The argument must be an iterable if specified.
+tuple #If no argument is given, the constructor returns an empty tuple. If iterable is specified the tuple is initialized from iterable's items. If the argument is a tuple, the return value is the same object.
+set #Build an unordered collection of unique elements.
+dict #
+
+#Classes = means custom data types
+
+#Specialized Data Types = special modules that can be added and used
+
+#None = nothing
